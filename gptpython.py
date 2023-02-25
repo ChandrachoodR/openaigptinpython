6077419691ee14cc5aabc53bae10a2c64c65973b
chandrachood Raveendran <103407699+ChandrachoodR@users.noreply.github.com>
2023-02-26 00:45:24 +0530

    time out correction

--- gptpython.py
+++ gptpython.py
@@ -1,42 +1,38 @@
-!pip install openai
 import openai
 import time
 
-# Set your OpenAI API key
-openai.api_key = "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"
+openai.api_key = "your_api_key_here"
 
-# Define a function to generate a follow-up question given a prompt
 def generate_followup(prompt):
     response = openai.Completion.create(
-        engine="davinci",
-        prompt=prompt,
-        max_tokens=64,
-        n=1,
-        stop=None,
-        temperature=0.5,
-        frequency_penalty=0.5,
-        presence_penalty=0.5
+      engine="davinci",
+      prompt=prompt,
+      max_tokens=64,
+      n=1,
+      stop=None,
+      temperature=0.5,
+      frequency_penalty=0.5,
+      presence_penalty=0.5
     )
+
     message = response.choices[0].text.strip()
     return message
 
-# Loop to receive user input and generate follow-up questions
 while True:
-    # Prompt user for input
     user_input = input("You: ")
-    
-    # Construct prompt for generating follow-up question
     prompt = "Ask a follow-up question to: \"" + user_input + "\""
-    
-    # Generate follow-up question using OpenAI API
+
     followup = generate_followup(prompt)
 
     # Wait for the API response to be ready
+    start_time = time.time()
     while followup == prompt:
-        print("Waiting for API response...")
-        time.sleep(0.5)
+        time.sleep(0.1)
+        elapsed_time = time.time() - start_time
+        if elapsed_time > 10:
+            print("Sorry, the API response timed out.")
+            break
         response = openai.Completion.fetch(response["id"])
         followup = response.choices[0].text.strip()
-    
-    # Print the generated follow-up question
+
     print("ChatGPT:", followup)
