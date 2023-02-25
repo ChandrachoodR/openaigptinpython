c246799b44af006e760d7e43f31249388b32cbdd
chandrachood Raveendran <103407699+ChandrachoodR@users.noreply.github.com>
2023-02-26 00:33:49 +0530

    waiting logic to ensure open ai response
    code updated to wait for open ai response.

--- gptpython.py
+++ gptpython.py
@@ -1,39 +1,42 @@
+!pip install opeanai
 import openai
 import time
 
-# Set OpenAI API key
-openai.api_key = "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"
+# Set your OpenAI API key
+openai.api_key = "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"
 
-# Function to generate a follow-up question
+# Define a function to generate a follow-up question given a prompt
 def generate_followup(prompt):
     response = openai.Completion.create(
-      engine="davinci",
-      prompt=prompt,
-      max_tokens=64,
-      n=1,
-      stop=None,
-      temperature=0.5,
-      frequency_penalty=0.5,
-      presence_penalty=0.5
+        engine="davinci",
+        prompt=prompt,
+        max_tokens=64,
+        n=1,
+        stop=None,
+        temperature=0.5,
+        frequency_penalty=0.5,
+        presence_penalty=0.5
     )
-
-    # Get the generated message from the OpenAI API response
     message = response.choices[0].text.strip()
     return message
 
-# Loop to continuously prompt for user input and generate follow-up questions
+# Loop to receive user input and generate follow-up questions
 while True:
+    # Prompt user for input
     user_input = input("You: ")
+    
+    # Construct prompt for generating follow-up question
     prompt = "Ask a follow-up question to: \"" + user_input + "\""
-
-    # Call generate_followup function to get the follow-up question
+    
+    # Generate follow-up question using OpenAI API
     followup = generate_followup(prompt)
 
     # Wait for the API response to be ready
     while followup == prompt:
-        time.sleep(0.1)
+        print("Waiting for API response...")
+        time.sleep(0.5)
         response = openai.Completion.fetch(response["id"])
         followup = response.choices[0].text.strip()
-
-    # Print the follow-up question
+    
+    # Print the generated follow-up question
     print("ChatGPT:", followup)
